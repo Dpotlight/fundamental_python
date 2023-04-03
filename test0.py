ef7f886cb3d8c0103c0b64ae1df7cb215e472763
DOHYEON KIM <dpotlight@naver.com>
2023-04-03 09:45:30 +0900

    Add files via upload

---
+++ test0.py
@@ -0,0 +1,9 @@
+print(19 + 2)
+print(29 - 3)
+print(23 * 4)
+print(25 / 5)
+print(26 // 3)
+print(53 % 4)
+
+print("Hello World")
+
---
+++ test0_idle.py
@@ -0,0 +1,100 @@
+Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/dogro/Desktop/파이썬프로그래밍(SW융합)(화)/script/20230314_2주차/test0.py
+21
+26
+92
+5.0
+8
+1
+Hello World
+'hello python!;
+SyntaxError: incomplete input
+'hello python!'
+'hello python!'
+print('hello python')
+hello python
+print('hello python!' , 10 * 10)
+hello python! 100
+'hello python!', 10 * 10
+('hello python!', 100)
+
+a = 100
+a
+100
+a * 8
+800
+a
+100
+
+================================ RESTART: Shell ================================
+a
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+
+a = 200
+int a;
+SyntaxError: invalid syntax
+b = 20.2
+b
+20.2
+a
+200
+a  + b
+220.2
+a * b
+4040.0
+a- b
+179.8
+a = 100.234
+a
+100.234
+a = ' hello world!'
+a
+' hello world!'
+print('3 X 5 = ', 3 * 5)
+3 X 5 =  15
+print('Hell' + 'o')
+Hello
+print('Hell','o')
+Hell o
+print('Hello'*10)
+HelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+'=' * 30
+'=============================='
+'=' * 60
+'============================================================'
+>>> '=' * 100
+'===================================================================================================='
+>>> '=' * 90
+'=========================================================================================='
+>>> '='* 80
+'================================================================================'
+>>> '=' * 79
+'==============================================================================='
+>>> '=' * 78
+'=============================================================================='
+>>> print ('100' +'100)
+...        
+SyntaxError: incomplete input
+>>> print('100' + '100')
+...        
+100100
+>>> print('100' * 5)
+...        
+100100100100100
+>>> print( 100*5)
+...        
+500
+>>> 
+= RESTART: /Users/dogro/Desktop/파이썬프로그래밍(SW융합)(화)/script/20230314_2주차/ftemptoctemp.py
+화씨온도 90 도를 섭씨로 바꾸면 32.22222222222222 도 입니다.
+>>> 
+= RESTART: /Users/dogro/Desktop/파이썬프로그래밍(SW융합)(화)/script/20230314_2주차/ftemptoctemp.py
+화씨온도 90 도를 섭씨로 바꾸면 32.22222222222222 도 입니다.
+>>> 
+= RESTART: /Users/dogro/Desktop/파이썬프로그래밍(SW융합)(화)/script/20230314_2주차/ftemptoctemp.py
+화씨온도 90 도를 섭씨로 바꾸면 32.22222222222222 도 입니다.
